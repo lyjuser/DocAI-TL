8721b01fc198ba6246ca5f3692ad0d487ae9d70b
Youjie Li <103523016+lyjuser@users.noreply.github.com>
2025-09-09 10:41:25 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -118,62 +118,7 @@ def make_coord(shape, ranges=None, flatten=True):
     return ret
 
 
-
 from sklearn.metrics import precision_recall_curve
-
-
-# def calc_f1(y_pred, y_true):
-#     batchsize = y_true.shape[0]
-#     with torch.no_grad():
-#         assert y_pred.shape == y_true.shape
-#         f1, auc, P, R = 0, 0, 0, 0
-#         y_true = y_true.cpu().numpy()
-#         y_pred = y_pred.cpu().numpy()
-#         for i in range(batchsize):
-#             true = y_true[i].flatten()
-#             true = true.astype(np.int)
-#             pred = y_pred[i].flatten()
-#
-#             precision, recall, thresholds = precision_recall_curve(true, pred)
-#
-#             # auc
-#             auc += roc_auc_score(true, pred)
-#             # auc += roc_auc_score(np.array(true>0).astype(np.int), pred)
-#             P += precision
-#             R += recall
-#             f1 += max([(2 * p * r) / (p + r+1e-10) for p, r in zip(precision, recall)])
-#
-#     return f1/batchsize, auc/batchsize, P/batchsize, R/batchsize
-
-# def calc_f1(y_pred, y_true):
-#     batchsize = y_true.shape[0]
-#     with torch.no_grad():
-#         assert y_pred.shape == y_true.shape
-#         f1_max, P, R, iou, auc = 0.0, 0.0, 0.0, 0.0, 0.0
-#         f1_mean = 0.0
-#         y_true = y_true.cpu().numpy()
-#         y_pred = y_pred.cpu().numpy()
-#         for i in range(batchsize):
-#             true = y_true[i].flatten()
-#             true = true.astype(np.int8)
-#             pred = y_pred[i].flatten()
-#
-#             precision, recall, thresholds = precision_recall_curve(true, pred)
-#             auc += roc_auc_score(true, pred)
-#
-#             inter = (pred * true).sum()
-#             union = (pred + true).sum() - inter
-#             iou += (inter / union)
-#
-#             P += precision.mean()
-#             R += recall.mean()
-#
-#             f1_max += max([(2 * p * r) / (p + r + 1e-8) for p, r in zip(precision, recall)])
-#
-#         f1_max, P, R, iou, auc = f1_max / batchsize, P / batchsize, R / batchsize, iou / batchsize, auc / batchsize
-#         f1_mean = (2 * P * R) / (P + R + 1e-8)
-#
-#     return f1_max, auc, iou, P, R, f1_mean
 
 def calc_f1(y_pred, y_true):
     batchsize = y_true.shape[0]
@@ -185,7 +130,6 @@ def calc_f1(y_pred, y_true):
         y_pred_numpy = y_pred.cpu().numpy()
 
         for i in range(batchsize):
-            # DTD methods
             pred_tamper = y_pred[i].squeeze(1)
             target_ = y_true[i].squeeze(1)
             match = (pred_tamper * target_).sum((1, 2))
@@ -194,12 +138,6 @@ def calc_f1(y_pred, y_true):
             precisions.append((match / (preds + 1e-8)).mean().item())
             recalls.append((match / (target_sum + 1e-8)).mean().item())
 
-            # print("match", match)
-            # print("preds", preds)
-            # print("target_sum", target_sum)
-            # print("precision", precisions)
-            # print("recall", recalls)
-            # SAM-Adapter methods
             true = y_true_numpy[i].flatten()
             true = true.astype(np.int8)
             pred = y_pred_numpy[i].flatten()
@@ -217,8 +155,5 @@ def calc_f1(y_pred, y_true):
         precisions = np.array(precisions).mean()
         recalls = np.array(recalls).mean()
         f = (2 * precisions * recalls) / (precisions + recalls + 1e-8)
-        # print("precisions", precisions)
-        # print("recalls", recalls)
-        # print("f", f)
-        # print("iou", iou)
     return f1_max, auc, iou, precisions, recalls, f
+
